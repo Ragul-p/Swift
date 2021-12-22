22464442c0665c1e7597698f9fa41e321f306f2c
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-12-22 20:26:55 +0530

    Update and rename 1. Variables and Constants.txt to 1. Variables and Constants.swift

---
+++ 1. Variables and Constants.swift
@@ -0,0 +1,48 @@
+//1.Basic
+
+   print(“hello world!”)
+   // commend
+
+
+
+
+//2
+//2.1 Swift Variables
+
+    Var num = 10
+//Declare Variables in Swift
+    Var number:Int 
+//Assign Values to Variables
+    Var name:String 
+    name = “ragul”
+
+
+
+//2.2 Swift constant
+
+    let number = 10
+    number=20  //error
+    
+
+
+
+//3.Rules for naming Swift Variables  
+    var a = "hello"
+    var _a = "hello"
+    var $a = "hello"
+    
+    var 1a = "hello" // throws error
+    
+    var A = 5 
+    var a = 55
+    print(A) // 5
+    print(a) // 55
+    
+
+
+//4. Swift Literals 
+
+    let height: Float = 6.4
+    let indian: Bool = true 
+    let initial: Character = "P"
+    let name: String = "Ragul" 
