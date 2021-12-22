ad12837e54ac0380db88665bfff7480623addfc6
Ragul-p <62152698+Ragul-p@users.noreply.github.com>
2021-12-22 20:35:33 +0530

    Update and rename 3. Character and String to 3. Character and String.swift

--- 3. Character and String.swift
+++ 3. Character and String.swift
@@ -1,4 +1,4 @@
-1.Swift Character :-
+//1.Swift Character :-
 
      var letter: Character
 
@@ -8,62 +8,64 @@
      let test: Character = "H@" //error
 
 
-2.Swift String :-
+
+
+
+//2.Swift String :-
 
      let name: String
 
      let name: String = "Swift"
      print(name)
 
-i.String Operations:-
 
-     1. Compare Two Strings:-
 
+ //i.String Operations:-
+
+  //1. Compare Two Strings:-
       let str1 = "Hello"
       let str2 = "Hello"
       print(str1 == str2)
 
-
-    2. Join Two Strings:-
-
+  //2. Join Two Strings:-
       var greet = "Hello"
       var name = "Jack"
       greet.append(name)
       print(greet)
 
-     // using + operator
+    // using + operator
         var result = greet + name
         print(result)
-
     //using =+ operator
+
        greet +=  name
        print(greet)
 
-   3. Find Length of String
-
-     let message = "Hello, World!"
-     print(message.count)
-
-ii.Escape Sequences:-
-
-    var example = "This is "String" class"
-    print(example) // throws error
-
-  var example = "This is \"String\" class"
-  print(example)
-  // Output: This is "String" class
+   //3. Find Length of String
+       let message = "Hello, World!"
+       print(message.count)
 
 
 
-iii.String Interpolation:-
+//ii.Escape Sequences:-
+       var example = "This is "String" class"
+       print(example) // throws error
+
+       var example = "This is \"String\" class"
+       print(example)
+       // Output: This is "String" class
+
+
+
+//iii.String Interpolation:-
     
-    let name = "Swift"
+      let name = "Swift"
 
-    var message = "This is \(name) programming."
-    print(message)
+      var message = "This is \(name) programming."
+      print(message)
 
 
-iv.Swift Multiline String:-
+//iv.Swift Multiline String:-
 
    // multiline string 
    var str: String = """
@@ -79,6 +81,6 @@ iv.Swift Multiline String:-
    """
 
 
-v.Create String Instance:-
+//v.Create String Instance:-
 
    var str = String()
